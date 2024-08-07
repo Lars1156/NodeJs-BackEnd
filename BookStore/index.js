3544a6b7211d0e871ff43bd0d480c2dd1db85fb5
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-08-07 09:46:14 +0530

    Add the datad base connectivity

--- BookStore/index.js
+++ BookStore/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { connection } = require('./connection');
 
 const app = express();
 
+// Database Coonectivty
+connection('').then(()=>{
+    console.log("Database Connected SucesssFully");   
+}).catch((error)=>{
+    console.log("Database is not connected", error);   
+});
+
+
 app.use(bodyParser.json())
 app.use(express.json());
-
 app.listen(5000 , ()=>{
     console.log("Server is listing on poert 5000");
     
