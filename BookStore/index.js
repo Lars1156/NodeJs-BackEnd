787a4295060eb989cd73b05a9be1a25eb147c522
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-08-07 10:21:46 +0530

    adding all user deatails

--- BookStore/index.js
+++ BookStore/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { connection } = require('./connection');
+const userRoutes = require('./routes/userRoutes')
 
 const app = express();
 
@@ -11,9 +12,10 @@ connection('mongodb://localhost:27017/BookShop').then(()=>{
     console.log("Database is not connected", error);   
 });
 
-
+app.use('/api/users', userRoutes);
 app.use(bodyParser.json())
 app.use(express.json());
+
 app.listen(5000 , ()=>{
     console.log("Server is listing on poert 5000");
     
