8039cace3dd1884e3f71b76d94e66f1b45099d18
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-08-07 10:14:11 +0530

    adding the user login and register

--- BookStore/controller/userController.js
+++ BookStore/controller/userController.js
@@ -1,2 +1,39 @@
 const User = require('../modules/userModule');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+exports.register = async (req, res) => {
+    const { username, password } = req.body;
+    console.log("**** User Login deatils" ,req.body);
+    try {
+      const user = new User({ username, password });
+      await user.save();
+  
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: '1h',
+      });
+  
+      res.status(201).json({ token });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
+
+  exports.login = async(res,req)=>{
+    exports.login = async (req, res) => {
+        const { username, password } = req.body;
+      
+        try {
+          const user = await User.findOne({ username });
+          if (!user || !(await user.matchPassword(password))) {
+            return res.status(401).json({ error: 'Invalid credentials' });
+          }
+      
+          const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '1h',
+          });
+      
+          res.status(200).json({ token });
+        } catch (error) {
+          res.status(400).json({ error: error.message });
+        }
+      };
+  }
