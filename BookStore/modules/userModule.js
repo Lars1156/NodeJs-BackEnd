34d4d935e51d0c3e293cc30bb61dc0e6e8df1016
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-08-07 10:00:31 +0530

    add the UserMode;

---
+++ BookStore/modules/userModule.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema =  new mongoose.Schema({
+    usrname:{type: String , required: true , unique: true},
+    password:{type: String , required: true}
+})
