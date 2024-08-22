33ad04cc8eead58273b6bc09e7943e562203d3f1
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-08-22 11:44:09 +0530

    add the res delete update

--- userInterface/services/restaurantService.js
+++ userInterface/services/restaurantService.js
@@ -18,3 +18,35 @@ exports.createRestaurant = async (restaurantData) =>{
 
   return restaurant;
 }
+// Get all restaurants
+exports.getAllRestaurants = async () => {
+    return await Restaurant.find();
+  };
+  
+  // Get a restaurant by ID
+  exports.getRestaurantById = async (restaurantId) => {
+    const restaurant = await Restaurant.findById(restaurantId);
+    if (!restaurant) {
+      throw new Error('Restaurant not found.');
+    }
+    return restaurant;
+  };
+  exports.updateRestaurant = async (restaurantId, updateData) => {
+    const restaurant = await Restaurant.findByIdAndUpdate(restaurantId, updateData, {
+      new: true,
+      runValidators: true,
+    });
+    if (!restaurant) {
+      throw new Error('Restaurant not found.');
+    }
+    return restaurant;
+  };
+  
+  // Delete a restaurant
+  exports.deleteRestaurant = async (restaurantId) => {
+    const restaurant = await Restaurant.findByIdAndDelete(restaurantId);
+    if (!restaurant) {
+      throw new Error('Restaurant not found.');
+    }
+    return restaurant;
+  };  
