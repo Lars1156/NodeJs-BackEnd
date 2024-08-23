eefd47be3f83539ff805c6fff5a1f07896eb9aaf
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-08-23 13:55:38 +0530

    add the menu controller

---
+++ userInterface/controller/menuController.js
@@ -0,0 +1,38 @@
+const menuService = require('../services/menuService');
+
+exports.createMenuItem = async (req, res) => {
+    try {
+      const menuItem = await menuService.createMenuItem(req.body);
+      res.status(201).json({ success: true, menuItem });
+    } catch (error) {
+      res.status(400).json({ success: false, message: error.message });
+    }
+  };
+
+  exports.getMenuByRestaurant = async (req, res) => {
+    try {
+      const menuItems = await menuService.getMenuByRestaurant(req.params.restaurantId);
+      res.status(200).json({ success: true, menuItems });
+    } catch (error) {
+      res.status(500).json({ success: false, message: error.message });
+    }
+  };
+
+  exports.getMenuItemById = async (req, res) => {
+    try {
+      const menuItem = await menuService.getMenuItemById(req.params.id);
+      res.status(200).json({ success: true, menuItem });
+    } catch (error) {
+      res.status(404).json({ success: false, message: error.message });
+    }
+  };
+
+  exports.updateMenuItem = async (req, res) => {
+    try {
+      const menuItem = await menuService.updateMenuItem(req.params.id, req.body);
+      res.status(200).json({ success: true, menuItem });
+    } catch (error) {
+      res.status(400).json({ success: false, message: error.message });
+    }
+  };
+  
