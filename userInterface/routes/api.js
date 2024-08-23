6b4ea364a1329033df1f61e770c6579b551e89f5
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-08-23 13:58:10 +0530

    creating a menau card route

--- userInterface/routes/api.js
+++ userInterface/routes/api.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const userController = require('../controller/userController');
-const { protect} = require('../middlewre/auth');
+const { protect ,admin} = require('../middlewre/auth');
 const router = express.Router();
 
 router.post('/registerUser',userController.registerUser);
 router.post('/loginUser', userController.loginUser);
 router.get('/:id', protect, userController.getUserById);
 router.put('/profile', protect, userController.updateUserProfile);
+
+// Menu routes
 
 module.exports = router;
